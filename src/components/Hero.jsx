f87cca42072f0169b49728f2b1161f2263fd05fd
Jenifer Tabita Ciuciu-Kiss <jenifer.tabita.ciuciu.kiss@gmail.com>
2024-11-13 16:17:55 +0100

    add new line after iswc2025

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,7 @@ export const Hero = () => {
       </div>
 
       <div class="relative z-10 flex flex-col justify-center items-center h-full text-center m-2 lg:m-0">
+        <h1 class="lg:text-5xl text-3xl font-semibold tracking-wide lg:font-[800] leading-tight mb-4">ISWC 2025</h1>
         <h1 class="lg:text-5xl text-3xl font-semibold tracking-wide lg:font-[800] leading-tight mb-4">ISWC 2025 THE 24th INTERNATIONAL SEMANTIC WEB CONFERENCE</h1>
         <p class="text-2xl font-semibold mb-6">Date:&nbsp;&nbsp;November 2-6,&nbsp;&nbsp;2025</p>
         <p class="text-2xl font-semibold mb-6">Nara,&nbsp;&nbsp;Japan</p>

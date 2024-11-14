db7b67265d297b2f9fa9b9e3f5838b0de00e1b85
Jenifer Tabita Ciuciu-Kiss <jenifer.tabita.ciuciu.kiss@gmail.com>
2024-11-14 16:08:53 +0100

    fix banner and add favicon

--- src/pages/committee.jsx
+++ src/pages/committee.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Sponsor from "../components/Sponsors/Sponsor";
 import organizers from "../components/assests/organizing/organizing"
+import banner from "../components/assests/ISWC2025banner.webp"
 
 
 const Committees = () => {
   return (
     <>
       <div className="flex justify-center items-center flex-col text-center" id="img">
+        <div className="w-full mb-8 pt-16 relative">
+          <img
+            src={ banner }
+            alt="Banner"
+            className="w-full h-auto object-cover"
+          />
+        </div>
         <Sponsor users={organizers} word="Organizing Committee" />
       </div>
     </>

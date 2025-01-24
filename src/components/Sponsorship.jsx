da8e78a31daff0201b0bf783f2b137b3d57cc35f
Nandana Mihindukulasooriya <nandana.cse@gmail.com>
2025-01-24 11:03:26 -0500

    a minor commit to test the workflow

--- src/components/Sponsorship.jsx
+++ src/components/Sponsorship.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export const Sponsorship = () => {
     return (
         <div className="container mx-auto px-4 lg:px-8 mt-12">
-            <h1 className="text-3xl font-bold text-center text-[#e94607]">Sponsorship</h1>
-            <p className="mt-4 text-lg text-center">Details about sponsorship opportunities.</p>
+            <h1 className="text-3xl font-bold text-center text-[#e94607]">Sponsorship</h1>      
+            <p className="mt-4 text-lg text-center">Details about sponsorship opportunities will be available soon.</p>
         </div>
     );
 };

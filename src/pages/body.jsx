cc93b64580747924ff76ccfae306105ecb278272
Jenifer Tabita Ciuciu-Kiss <jenifer.tabita.ciuciu.kiss@gmail.com>
2025-05-01 13:16:07 +0200

    remove dates from landing page

--- src/pages/body.jsx
+++ src/pages/body.jsx
@@ -18,13 +18,13 @@ const Body = () => {
         </div>
       </div>
       <About />
-      <div className="grid grid-cols-1 lg:grid-cols-8 gap-x-0 mx-2 my-10 ">
-        <div className="m-2  lg:col-span-8 flex flex-col justify-center items-center shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]">
+      {/* <div className="grid grid-cols-1 lg:grid-cols-8 gap-x-0 mx-2 my-10 "> */}
+        {/* <div className="m-2  lg:col-span-8 flex flex-col justify-center items-center shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]">
           <div class="text-white mt-8  bg-[#e94607] w-2/3 py-2 text-center rounded-lg text-lg font-semibold transition  ">
             Important Dates
           </div>
           <Timeline />
-        </div>
+        </div> */}
         {/* <div className="m-4 lg:col-span-3 w-90%  bg-[#33358c] flex justify-center items-center shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px]">
           <Guidelines />
         </div>
@@ -33,7 +33,7 @@ const Body = () => {
             Announcements
           </div>
         </div> */}
-      </div>
+      {/* </div> */}
       <div className="mx-auto flex flex-col justify-center items-center">
 
       </div>

6a26354f53e9153c59f50cd51aeee50022a3ade0
Victor Eiti Yamamoto <eitiyamamoto@gmail.com>
2025-05-26 11:50:31 +0900

    Added new sponsor

--- src/components/sponsorship/Sponsors.jsx
+++ src/components/sponsorship/Sponsors.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import google_logo from "./assets/google_logo.png"
+import lore_star_logo from "./assets/lore_star_logo.png"
 import './sponsorship.css';
 
 export const Sponsors = () => {
@@ -12,7 +14,7 @@ export const Sponsors = () => {
                     <h2 className="text-3xl font-bold text-[#e94607]">Gold</h2>
                     <hr className="border-t-2 border-[#e94607] my-2" />
                     <div className="sponsor-info mt-4">
-                        <h3 className="text-lg font-semibold">eBay</h3>
+                        <h3 className="text-lg font-semibold text-center">eBay</h3>
                         <div className="sponsor-logos flex justify-center items-center gap-8">
                             <a href="https://jobs.ebayinc.com/us/en/" target="_blank" rel="noopener noreferrer">
                                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/EBay_logo.svg/880px-EBay_logo.svg.png" alt="ebay Logo" className="logo-high-res" width="300"/>
@@ -20,21 +22,31 @@ export const Sponsors = () => {
                         </div>
                     </div>
                     <div className="sponsor-info mt-4">
-                        <h3 className="text-lg font-semibold">Google</h3>
+                        <h3 className="text-lg font-semibold text-center">Google</h3>
                         <div className="sponsor-logos flex justify-center items-center gap-8">
                             <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-                                <img src="https://www.dropbox.com/scl/fi/kccump28axk83iryyxtef/googlelogo_color_416x140dp.png?rlkey=72f2bwfqk9ngeyn2flv81p1kr&dl=1" alt="ebay Logo" className="logo-high-res" width="300"/>
+                                <img src={google_logo} alt="Google Logo" className="logo-high-res" width="300"/>
                             </a>
                         </div>
                     </div>
 
                     <h3 className="text-lg font-semibold">Student Support</h3>
-                        <p className="text-sm mb-4">Sponsored by: <em>Artificial Intelligence Journal</em></p>
-                        <div className="sponsor-logos flex justify-center items-center gap-8">
-                            {/* <a href="https://aij.ijcai.org/wp-content/uploads/2021/07/ARTINT_Logo2_c_highresolution.tif" target="_blank" rel="noopener noreferrer"> */}
-                                <img src="https://aij.ijcai.org/wp-content/uploads/2021/07/ARTINT_Logo2_c_web_more.jpg" alt="Artificial Intelligence Journal Logo" className="logo-high-res"/>
-                            {/* </a> */}
-                        </div>
+                    <p className="text-sm mb-4">Sponsored by: <em>Artificial Intelligence Journal</em></p>
+                    <h3 className="text-lg font-semibold text-center">Artificial Intelligence Journal</h3>
+                    <div className="sponsor-logos flex justify-center items-center gap-8">
+                        {/* <a href="https://aij.ijcai.org/wp-content/uploads/2021/07/ARTINT_Logo2_c_highresolution.tif" target="_blank" rel="noopener noreferrer"> */}
+                            <img src="https://aij.ijcai.org/wp-content/uploads/2021/07/ARTINT_Logo2_c_web_more.jpg" alt="Artificial Intelligence Journal Logo" className="logo-high-res"/>
+                        {/* </a> */}
+                    </div>
+                    
+                    <h2 className="text-3xl font-bold text-[#e94607]">Silver</h2>
+                    <hr className="border-t-2 border-[#e94607] my-2" />
+                    <h3 className="text-lg font-semibold text-center">Lore Star</h3>
+                    <div className="sponsor-logos flex justify-center items-center gap-8">
+                        <a href="https://lorestar.it/" target="_blank" rel="noopener noreferrer">
+                            <img src={lore_star_logo} alt="Lore Star Logo" className="logo-high-res" width="300"/>
+                        </a>
+                    </div>
 
                     {/* <h2 className="text-2xl font-bold text-[#e94607]">Bronze</h2>
                     <hr className="border-t-2 border-[#e94607] my-2" />

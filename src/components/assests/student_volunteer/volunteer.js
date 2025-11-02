49f6cee09e367ab72e374ffe940557af07d2329b
Victor Eiti Yamamoto <eitiyamamoto@gmail.com>
2025-11-02 10:08:18 +0900

    Added more volunteers

--- src/components/assests/student_volunteer/volunteer.js
+++ src/components/assests/student_volunteer/volunteer.js
@@ -1,5 +1,13 @@
 import yamamoto from "./yamamoto.jpg"
 import nishigori from "./nishigori.jpeg"
+import zhao from "./zhao.png"
+import omiya from "./omiya.jpeg"
+import he from "./he.png"
+import yokoyama from "./yokoyama.jpeg"
+import ando from "./ando.jpeg"
+import yamada from "./yamada.png"
+import lakshan from "./lakshan.png"
+import sookplang from "./sookplang.jpeg"
 
 const volunteers ={
     "Website": [
@@ -24,7 +32,62 @@ const volunteers ={
             country: 'Japan',
         },
     ],
-
+    "On-site Staff": [
+        {
+            name: "Shixiong ZHAO",
+            img: zhao,
+            institution: 'National Institute of Informatics / The Graduate University for Advanced Studies, SOKENDAI',
+            country: 'Japan',
+        },
+        {
+            name: "Takeshi Omiya",
+            img: omiya,
+            institution: "University of Tokyo",
+            country: 'Japan',
+        },
+        {
+            name: "Yanming He",
+            img: he,
+            institution: 'National Institute of Informatics / The Graduate University for Advanced Studies, SOKENDAI',
+            country: 'Japan',
+        },
+        {
+            name: "Haruki Yokoyama",
+            img: yokoyama,
+            institution: 'The University of Electro-Communications',
+            country: 'Japan',
+        },
+        {
+            name: "Taichi Ando",
+            img: ando,
+            institution: 'The University of Electro-Communications',
+            country: 'Japan',
+        },
+        {
+            name: "Shintaro Yamada",
+            img: yamada,
+            institution: 'University of Tokyo',
+            country: 'Japan',
+        },
+        {
+            name: 'Kotaro Nishigori',
+            img: nishigori,
+            institution: 'National Institute of Informatics / The Graduate University for Advanced Studies, SOKENDAI',
+            country: 'Japan',
+        },
+        {
+            name: 'Lakshan Karunathilake',
+            img: lakshan,
+            institution: 'National Institute of Informatics / The Graduate University for Advanced Studies, SOKENDAI',
+            country: 'Japan',
+        },
+        {
+            name: 'Sookplang Vidchaphol',
+            img: sookplang,
+            institution: 'Japan Advanced Institute of Science and Technology',
+            country: 'Japan',
+        },
+    ]
 };
 
 export default volunteers;

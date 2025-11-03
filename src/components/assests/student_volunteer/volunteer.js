a498560f8ae8c74f255f685d0a2063e77db16434
Victor Eiti Yamamoto <eitiyamamoto@gmail.com>
2025-11-03 09:16:00 +0900

    Updated volunteer list

--- src/components/assests/student_volunteer/volunteer.js
+++ src/components/assests/student_volunteer/volunteer.js
@@ -8,6 +8,17 @@ import ando from "./ando.jpeg"
 import yamada from "./yamada.png"
 import lakshan from "./lakshan.png"
 import sookplang from "./sookplang.jpeg"
+import tanaka from "./tanaka.jpeg"
+import maeda from "./maeda.jpeg"
+import shimoide from "./shimoide.jpeg"
+import yasui from "./yasui.jpeg"
+import sakamoto from "./sakamoto.jpeg"
+import hotta from "./hotta.jpeg"
+import takamatsu from "./takamatsu.jpeg"
+import fukuda from "./fukuda.jpeg"
+import doi from "./doi.jpeg"
+import marui from "./marui.jpeg"
+import yanagi from "./yanagi.jpeg"
 
 const volunteers ={
     "Website": [
@@ -85,6 +96,72 @@ const volunteers ={
             name: 'Sookplang Vidchaphol',
             img: sookplang,
             institution: 'Japan Advanced Institute of Science and Technology',
+            country: 'Japan',
+        },
+        {
+            name: 'Atsumi Tanka',
+            img: tanaka,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Ryo Maeda',
+            img: maeda,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Ren Shimoide',
+            img: shimoide,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Masahito Yasui',
+            img: yasui,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Takumi Sakamoto',
+            img: sakamoto,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Takehiro Hotta',
+            img: hotta,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Yuya Takamatsu',
+            img: takamatsu,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Shintaro Fukuda',
+            img: fukuda,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Kohei Doi',
+            img: doi,
+            institution: 'Osaka Electro-Communication University',
+            country: 'Japan',
+        },
+        {
+            name: 'Yui Marui',
+            img: marui,
+            institution: 'University of Hyogo',
+            country: 'Japan',
+        },
+        {
+            name: 'Yui Yanagi',
+            img: yanagi,
+            institution: 'University of Hyogo',
             country: 'Japan',
         },
     ]

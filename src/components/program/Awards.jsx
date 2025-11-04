dec449c12a47b5eb46c0299ed3e52069b2c387f1
Victor Eiti Yamamoto <eitiyamamoto@gmail.com>
2025-11-04 11:41:24 +0900

    Added ten-year award

--- src/components/program/Awards.jsx
+++ src/components/program/Awards.jsx
@@ -21,8 +21,10 @@ export const Awards = () => {
                     </section>
 
                     <section className="mb-6">
-                        <h2 className="text-2xl font-semibold">Test-of-time Paper Award 2025</h2>
-                        <p>To be announced at the opening ceremony of the conference..</p>
+                        <h2 className="text-2xl font-semibold">SWSA Ten-Year Award 2025</h2>
+                        <p>The recipient of this year's <a href="https://swsa.semanticweb.org/content/swsa-ten-year-award">SWSA Ten-Year Award</a> is </p>
+                        <p><a href="https://link.springer.com/chapter/10.1007/978-3-319-25010-6_1">RDFox: A Highly-Scalable RDF Store</a>, by Yavor Nenov, Robert Piro, Boris Motik, Ian Horrocks, Zhe Wu, and Jay Banerjee.</p>
+                        <p>Nenov, Y., Piro, R., Motik, B., Horrocks, I., Wu, Z., Banerjee, J. (2015). RDFox: A Highly-Scalable RDF Store. In: Arenas, M., et al. The Semantic Web - ISWC 2015. ISWC 2015. Lecture Notes in Computer Science(), vol 9367. Springer, Cham. https://doi.org/10.1007/978-3-319-25010-6_1</p>
                     </section>
 
                     <section className="mb-6">
